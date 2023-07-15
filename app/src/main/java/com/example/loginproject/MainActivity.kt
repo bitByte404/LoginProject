5bfef49bc6997c3f4e36673aed5cdc12d07b1cc4
pxd <932056657@qq.com>
2023-07-15 11:07:14 +0800

    USerManager 用户操作

--- app/src/main/java/com/example/loginproject/MainActivity.kt
+++ app/src/main/java/com/example/loginproject/MainActivity.kt
@@ -4,20 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.loginproject.file.FileManager
+import com.example.loginproject.user.PasswordType
 import com.example.loginproject.user.User
+import com.example.loginproject.user.UserManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val users = listOf(
-            User("jack","123","456",false),
-            User("rose","252","247",false)
-        )
-        FileManager.sharedInstance().readData(this).also {
-            it.forEach { user ->
-                Log.v("pxd","$user")
-            }
-        }
+
+        UserManager.sharedInstance(this).loadAllUserInfo()
+        UserManager.sharedInstance(this).login("jack","123",PasswordType.LoginType_Pin)
     }
 }
---
+++ app/src/main/java/com/example/loginproject/user/PasswordType.kt
@@ -0,0 +1,6 @@
+package com.example.loginproject.user
+
+object PasswordType {
+    const val LoginType_Pin = 1
+    const val LoginType_Pattern = 2
+}
